97848840b9e103bc3d9144acec14079319430deb
fmelods <felipemelodesousa4@gmail.com>
2025-03-26 18:05:58 -0300

    Adicionando estilos nas navegações

--- nav-router-app/app/_Layout.tsx
+++ nav-router-app/app/_Layout.tsx
@@ -1,15 +1,18 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
 import Header from '../components/Header';
 
 import "../global.css";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 const RootLayout = () => {
   return (
-    <>
+    <SafeAreaView className="flex-1 bg-blue-500">
     <Header />
     <Slot />
-    </>
+    <StatusBar translucent backgroundColor='transparent' style="auto"/>
+    </SafeAreaView>
   );
 }
 
